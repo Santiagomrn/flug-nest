63b7d6f21256d24ebc18499f93f24e6746de2081
Santiago Rodriguez <rodrigueznohsantiagomiguel@gmail.com>
2024-08-11 23:11:16 -0600

    refactor(logger): improve object print

--- src/core/logger/Logger.ts
+++ src/core/logger/Logger.ts
@@ -8,49 +8,106 @@ export class Logger implements LoggerService {
   /**
    * Write a 'log' level log.
    */
-  log(...message: any) {
-    winstonLogger.info(`[${this.context}] ` + message.join(''));
+  log(message: any, ...optionalParams: any[]) {
+    if (typeof message === 'object' && message !== null)
+      message = JSON.stringify(message);
+    optionalParams = optionalParams.map((optionalParam) => {
+      if (typeof optionalParam === 'object' && optionalParam !== null)
+        return JSON.stringify(optionalParam);
+      return optionalParam;
+    });
+    winstonLogger.info(
+      `[${this.context}] ` + message + optionalParams.join(''),
+    );
   }
-  info(...message: any) {
-    winstonLogger.info(`[${this.context}] ` + message.join(''));
+  info(message: any, ...optionalParams: any[]) {
+    if (typeof message === 'object' && message !== null)
+      message = JSON.stringify(message);
+    optionalParams = optionalParams.map((optionalParam) => {
+      if (typeof optionalParam === 'object' && optionalParam !== null)
+        return JSON.stringify(optionalParam);
+      return optionalParam;
+    });
+    winstonLogger.info(
+      `[${this.context}] ` + message + optionalParams.join(''),
+    );
   }
   /**
    * Write a 'fatal' level log.
    */
-  fatal(...message: any) {
-    winstonLogger.error(`[${this.context}] ` + message.join(''));
+  fatal(message: any, ...optionalParams: any[]) {
+    if (typeof message === 'object' && message !== null)
+      message = JSON.stringify(message);
+    winstonLogger.error(
+      `[${this.context}] ` + message + optionalParams.join(''),
+    );
   }
 
   /**
    * Write an 'error' level log.
    */
-  error(...message: any) {
-    winstonLogger.error(`[${this.context}] ` + message.join(''));
+  error(message: any, ...optionalParams: any[]) {
+    if (typeof message === 'object' && message !== null)
+      message = JSON.stringify(message);
+    optionalParams = optionalParams.map((optionalParam) => {
+      if (typeof optionalParam === 'object' && optionalParam !== null)
+        return JSON.stringify(optionalParam);
+      return optionalParam;
+    });
+    winstonLogger.error(
+      `[${this.context}] ` + message + optionalParams.join(''),
+    );
   }
 
   /**
    * Write a 'warn' level log.
    */
-  warn(...message: any) {
-    winstonLogger.warn(`[${this.context}] ` + message.join(''));
+  warn(message: any, ...optionalParams: any[]) {
+    if (typeof message === 'object' && message !== null)
+      message = JSON.stringify(message);
+    optionalParams = optionalParams.map((optionalParam) => {
+      if (typeof optionalParam === 'object' && optionalParam !== null)
+        return JSON.stringify(optionalParam);
+      return optionalParam;
+    });
+    winstonLogger.warn(
+      `[${this.context}] ` + message + optionalParams.join(''),
+    );
   }
 
   /**
    * Write a 'debug' level log.
    */
-  debug(...message: any) {
-    winstonLogger.debug(`[${this.context}] ` + message.join(''));
+  debug(message: any, ...optionalParams: any[]) {
+    if (typeof message === 'object' && message !== null)
+      message = JSON.stringify(message);
+    optionalParams = optionalParams.map((optionalParam) => {
+      if (typeof optionalParam === 'object' && optionalParam !== null)
+        return JSON.stringify(optionalParam);
+      return optionalParam;
+    });
+    winstonLogger.debug(
+      `[${this.context}] ` + message + optionalParams.join(''),
+    );
   }
 
   /**
    * Write a 'verbose' level log.
    */
-  verbose?(...message: any) {
-    winstonLogger.verbose(`[${this.context}] ` + message.join(''));
+  verbose?(message: any, ...optionalParams: any[]) {
+    if (typeof message === 'object' && message !== null)
+      message = JSON.stringify(message);
+    winstonLogger.verbose(
+      `[${this.context}] ` + message + optionalParams.join(''),
+    );
   }
 
-  http(...message: any) {
-    winstonLogger.http(`[${this.context}] ` + message.join(''));
+  http(message: any, ...optionalParams: any[]) {
+    if (typeof message === 'object' && message !== null)
+      message = JSON.stringify(message);
+    winstonLogger.http(
+      `[${this.context}] ` + message + optionalParams.join(''),
+    );
   }
 }
 export const logger = new Logger();
