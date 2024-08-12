a5d4a5f9d1c82a2a3c502d4e0745e83342f637c4
Santiago Rodriguez <rodrigueznohsantiagomiguel@gmail.com>
2024-08-11 23:16:34 -0600

    chore(decorators): add generic types

--- src/common/decorators/appendUser.decorator.ts
+++ src/common/decorators/appendUser.decorator.ts
@@ -1,7 +1,9 @@
 import { SetMetadata, UseInterceptors, applyDecorators } from '@nestjs/common';
 import { AppendUserInterceptor } from '@interceptors/appendUser.interceptor';
 
-export function AppendUser(appendUserToBodyKey: string = 'userId') {
+export function AppendUser<T>(
+  appendUserToBodyKey: keyof T | 'userId' = 'userId',
+) {
   return applyDecorators(
     SetMetadata('appendUserToBodyKey', appendUserToBodyKey),
     UseInterceptors(AppendUserInterceptor),
--- src/modules/auth/decorators/isOwner.decorator.ts
+++ src/modules/auth/decorators/isOwner.decorator.ts
@@ -1,8 +1,13 @@
 import { SetMetadata, Type, UseGuards, applyDecorators } from '@nestjs/common';
 import { IsOwnerGuard } from '../guards/isOwner.guard';
 import { IFindOne } from '@libraries/intefaces/IFindOne';
+import { Plain } from '@libraries/baseModel.entity';
+import { Model } from 'sequelize-typescript';
 
-export function IsOwner(serviceToken: Type<IFindOne>, key: string = 'userId') {
+export function IsOwner<T extends Model<T>>(
+  serviceToken: Type<IFindOne>,
+  key: keyof Plain<T> | 'userId' = 'userId',
+) {
   return applyDecorators(
     SetMetadata('serviceToken', serviceToken),
     SetMetadata('key', key),
