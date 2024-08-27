baf881f3fcfb620e4d28248e423fabeb27fa2609
Santiago Rodriguez <rodrigueznohsantiagomiguel@gmail.com>
2024-08-27 12:18:32 -0600

    refactor(decorators): type safety for filterOwner decorator

--- src/common/decorators/filterOwner.decorator.ts
+++ src/common/decorators/filterOwner.decorator.ts
@@ -1,7 +1,11 @@
 import { SetMetadata, UseInterceptors, applyDecorators } from '@nestjs/common';
 import { FilterOwnerInterceptor } from '@interceptors/filterOwner.interceptor';
+import { Plain } from '@libraries/baseModel.entity';
+import { Model } from 'sequelize-typescript';
 
-export function FilterOwner(key: string = 'userId') {
+export function FilterOwner<T extends Model>(
+  key: keyof Plain<T> | 'userId' = 'userId',
+) {
   return applyDecorators(
     SetMetadata('key', key),
     UseInterceptors(FilterOwnerInterceptor),
