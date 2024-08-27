04a1612c18350381e65afc5b816d23708a863dfb
Santiago Rodriguez <rodrigueznohsantiagomiguel@gmail.com>
2024-08-27 11:49:37 -0600

    feat(validationPipe): set exposeDefaultValues as true

--- src/main.ts
+++ src/main.ts
@@ -92,6 +92,9 @@ async function bootstrap() {
     new ValidationPipe({
       whitelist: true,
       transform: true,
+      transformOptions: {
+        exposeDefaultValues: true,
+      },
       validateCustomDecorators: true, //https://docs.nestjs.com/custom-decorators
     }),
   );
