37437e74a31ac865619231d0c17ce72f1d6196b6
Santiago Rodriguez <rodrigueznohsantiagomiguel@gmail.com>
2024-08-28 10:52:15 -0600

    refactor(decorator): add type safety for appendToBody decorator

--- src/common/decorators/appendToBody.decorator.ts
+++ src/common/decorators/appendToBody.decorator.ts
@@ -1,7 +1,7 @@
 import { SetMetadata, UseInterceptors, applyDecorators } from '@nestjs/common';
 import { AppendToBodyInterceptor } from '@interceptors/appendToBody.interceptor';
 
-export function AppendToBody(appendToBodyData: object = {}) {
+export function AppendToBody<T>(appendToBodyData: Partial<T> = {}) {
   return applyDecorators(
     SetMetadata('appendToBodyData', appendToBodyData),
     UseInterceptors(AppendToBodyInterceptor),
